44d7d4701023128d6ffa92d49b4c8c629d073662
Umwizer <ruthmwizerwa@gmail.com>
2025-01-28 19:57:31 +0200

    codewars katas

---
+++ ES6/script.js
@@ -0,0 +1,19 @@
+//Const keyword
+
+// window.onload = function () {
+//    const pi = 3.142;
+//    function calcArea(r){
+//     console.log("The area is :"+pi * r * r)
+//    }
+//    calcArea(5);
+// };
+
+window.onload = function(){
+    var x=10;
+    if(x>5){
+        let  x = 5;
+        console.log("inside:" + x)
+
+    }
+    console.log("outside:" + x)
+}
