acfb49eb1c88a6d061c88bc099fca00f17bbd006
Umwizer <ruthumwizerwa@gmail.com>
2025-02-16 14:59:02 +0200

    greet

---
+++ programming/Create a Greeting Function .js
@@ -0,0 +1,9 @@
+// Write a JavaScript function named greet that takes a string parameter name and prints a greeting message. 
+// Then, call the function with "Alice" as the argument.
+function greet(name){
+     console.log("Hello, " + name + "!");
+}
+
+greet("Alice");
+
+
