fd8d4b0fc333499d064fee6f1ba1e17d132dd0f1
Umwizer <ruthumwizerwa@gmail.com>
2025-02-24 10:23:37 +0200

    spreadOperator

---
+++ ES6/SpreadOperator.js
@@ -0,0 +1,15 @@
+// // ### **Spread Operator (`...`)**
+
+// // The spread operator **expands** an array or object into individual elements.
+
+
+// Spreading an Array
+
+const arr1 = [1,2,3]
+const arr2 = [...arr1,4,5]
+console.log(arr2)
+// Spreading an Object
+
+const obj1 = {name:"Ruth",age:22}
+const obj2 = {...obj1,country:"Rwanda"}
+console.log(obj2)
