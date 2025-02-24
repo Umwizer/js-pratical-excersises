477b2e154a5354715d79926c393b8dec9a41fca9
Umwizer <ruthumwizerwa@gmail.com>
2025-02-24 09:37:41 +0200

    promises

---
+++ ES6/Promises.js
@@ -0,0 +1,9 @@
+// Promise in JavaScript : is an object that represents the eventual  completion or failure of asynchronous
+//operation and it's operating value
+
+// state of promise 
+// 1. Pending : The initial state ,operation is still in progress
+// 2. Fulfilled : The operation completed successfully and result is available
+// 3. Rejected : The operation failed and error reason is available
+
+
