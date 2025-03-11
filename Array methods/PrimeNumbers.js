0ae4cad31f8a42980623de4a0846740c6386817c
Umwizer <ruthumwizerwa@gmail.com>
2025-03-11 09:47:35 +0200

    filterlearning

--- Array methods/PrimeNumbers.js
+++ Array methods/PrimeNumbers.js
@@ -1,2 +1,16 @@
 // Write a function called filterPrimeNumbers that filters prime numbers from an array.
 //  A prime number is only divisible by 1 and itself.
+function filterPrimeNumbers(arr) {
+    return arr.filter(num => {
+        if (num <= 1) {
+          return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+          if (num % i === 0) {
+            return false;
+          }
+        }
+        return true;
+      });
+  
+  }
