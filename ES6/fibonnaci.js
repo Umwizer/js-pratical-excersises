5130e4bce3c7a9d75e528142a1aecfe183f9dcdb
Umwizer <ruthumwizerwa@gmail.com>
2025-04-14 09:32:40 +0200

    fibonnaci sequence

--- ES6/fibonnaci.js
+++ ES6/fibonnaci.js
@@ -2,3 +2,17 @@
 // fibonacciGenerator(limit) that yields Fibonacci
 // umbers up to a specified limit. Remember, the first two
 // Fibonacci numbers are 0 and 1, and each subsequent number is the sum of the previous two.
+
+function fibonacciUpTo(limit) {
+  let a = 0;
+  let b = 1;
+
+  while (a <= limit) {
+    console.log(a);
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+}
+
+fibonacciUpTo(50);
