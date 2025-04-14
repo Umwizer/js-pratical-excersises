9649038ea0dcfcfd04c260e1c75dc805ed038b02
Umwizer <ruthumwizerwa@gmail.com>
2025-04-14 14:56:58 +0200

    nested callback

---
+++ Asynchronous and Event-Based Programming/callbacks/NestedCallBack.js
@@ -0,0 +1,28 @@
+// ✅ Nested callbacks
+// Create three functions:
+
+// washHands(callback)
+
+// eatDinner(callback)
+
+// goToSleep()
+
+// Call them in order using nested callbacks (callback hell style).
+
+function washHands(callback) {
+  setTimeout(() => {
+    console.log("Hands washed");
+    callback();
+  }, 1000);
+}
+function eatDinner(callback) {
+  setTimeout(() => {
+    console.log("Taking dinner");
+    callback();
+  }, 2000);
+}
+function goToSleep() {
+  setTimeout(() => {
+    console.log("Sleeping");
+  }, 3000);
+}
