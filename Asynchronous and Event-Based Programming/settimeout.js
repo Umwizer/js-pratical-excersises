98f664f30dbcbfa9e98b62a01ef0cc709aac0446
Asifiwe Buhendwa <ruthumwizerwa@gmail.com>
2025-04-22 16:59:11 +0200

    settimeout

---
+++ Asynchronous and Event-Based Programming/settimeout.js
@@ -0,0 +1,6 @@
+const delayedHello = () => {
+    console.log('Hi! This is an asynchronous greeting!');
+  };
+  
+  setTimeout(delayedHello, 2000);
+// setTimeout() has two callback function and delay in milliseconds  
