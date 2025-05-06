8ab1eac0d5edfc786b50896b126ea638ed6f10fb
Umwizer <ruthumwizerwa@gmail.com>
2025-05-06 10:52:15 +0300

    languages kata

---
+++ Codewars/Get LocalizedLanguageName.js
@@ -0,0 +1,22 @@
+// Task Description
+// Implement a function getLocalizedLanguageName that returns the localized name of a language.
+
+// Requirements
+// Input:
+// The function receives two parameters:
+
+// displayLocale: a string representing the language code of the locale in which the language name should be displayed.
+
+// languageCode: a string representing the language code of the language being translated.
+
+// It is guaranteed that all language codes adhere to the ISO 639 standard.
+
+// Functionality:
+// The function should return the name of the language corresponding to languageCode, localized according to displayLocale.
+
+// Examples:
+// getLocalizedLanguageName('en', 'ru'); // should return "Russian"
+// getLocalizedLanguageName('es', 'de'); // should return "alemán"
+// getLocalizedLanguageName('fr', 'ja'); // should return "japonais"
+// getLocalizedLanguageName('pt', 'it'); // should return "italiano"
+// getLocalizedLanguageName('zh', 'ar'); // should return "阿拉伯语"
