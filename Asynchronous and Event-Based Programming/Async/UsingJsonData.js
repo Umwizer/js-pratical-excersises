ae7e5b88a4790cad7c001676846b439d0929e5d5
Umwizer <ruthumwizerwa@gmail.com>
2025-05-20 15:22:51 +0300

    TodoJson.Js

--- Asynchronous and Event-Based Programming/Async/UsingJsonData.js
+++ Asynchronous and Event-Based Programming/Async/UsingJsonData.js
@@ -2,12 +2,13 @@ const getTodos = (callback) => {
   const request = new XMLHttpRequest();
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       callback("could not fetch data", undefined);
     }
   });
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.open("GET", "todos.json");
   request.send();
 };
 console.log(1);
