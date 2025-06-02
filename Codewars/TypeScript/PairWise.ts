5ca9adb16e5da0975a04bd3a567e394cdb12ea19
Umwizer <ruthumwizerwa@gmail.com>
2025-06-02 12:32:29 +0300

    asyn

---
+++ Codewars/TypeScript/PairWise.ts
@@ -0,0 +1,11 @@
+// // Total Pairwise Absolute Difference
+// The Disparity of Numeria
+// In the Kingdom of Numeria, every citizen is known by a number. King Numerus, always fascinated by numbers, wanted to measure the “disparity” among his subjects. He defined it as the sum of absolute differences between every pair of citizens' numbers.
+
+// At first, his advisors attempted a slow, pair-by-pair approach, but it took far too long. Then, the brilliant mathematician Arithmo found a hidden pattern that allowed him to compute the total disparity much more efficiently.
+
+// Can you uncover Arithmo’s secret and calculate the total disparity in an optimal way?
+
+// Your task is to write a function that takes a list of citizen numbers and returns the sum of all absolute differences between every pair of numbers.
+
+// Good luck discovering the secret to Numeria’s harmony!
