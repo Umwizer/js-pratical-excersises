66f00712e28615adf3b6059f2c6c28c49b85ec56
Umwizer <ruthumwizerwa@gmail.com>
2025-06-09 23:18:23 +0200

    async

---
+++ Asynchronous and Event-Based Programming/abortController.js
@@ -0,0 +1,4 @@
+// mplement a timeout for an asynchronous fetch request.
+// If the request takes longer than(5 milliseconds), it should be aborted.
+
+// https://jsonplaceholder.typicode.com/users
---
+++ Asynchronous and Event-Based Programming/fromCallback.js
@@ -0,0 +1,35 @@
+// Write a JavaScript program that converts this callback-based function to a promise-based function.
+
+// function fetchData(callback) {
+//     setTimeout(() => {
+//       const data = "Data fetched successfully!";
+//       callback(null, data);
+//     }, 1000);
+//   }
+
+//   fetchData((error, data) => {
+//     if (error) {
+//       console.error("Error:", error);
+//     } else {
+//       console.log(data);
+//     }
+//   });
+
+async function fetchD(callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = "data successfully";
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    }, 1000);
+  });
+}
+fetchD()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
