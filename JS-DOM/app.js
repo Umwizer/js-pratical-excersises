a7fbf1df1e1cd597aafc1d4ecceb1f93a3ece6ac
Umwizer <ruthumwizerwa@gmail.com>
2025-06-14 10:55:42 +0200

    getelementsbyclassname

--- JS-DOM/app.js
+++ JS-DOM/app.js
@@ -4,7 +4,8 @@
 const title = document.getElementById("main-heading");
 console.log(title);
 //GetElementByClassName()
-
+const listItem = document.getElementsByClassName("list-items");
+console.log(listItem);
 //getElementByTagName()
 
 //querySelector()
