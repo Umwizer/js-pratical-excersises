4b95bb62a8f05408dc7843c15f7bed47bb3c1c29
Umwizer <ruthumwizerwa@gmail.com>
2025-09-08 16:11:40 +0200

    node crash course

---
+++ node-crash-course/global.js
@@ -0,0 +1,11 @@
+global.setTimeout(() => {
+  console.log("In theTimeout ");
+  clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+  console.log("Set TimeInterval");
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
