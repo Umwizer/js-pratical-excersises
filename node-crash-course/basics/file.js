88df2ed1880860d0d13d478e4d565a81e9df395c
Umwizer <ruthumwizerwa@gmail.com>
2025-09-15 11:41:44 +0200

    directories and deleting files

--- node-crash-course/basics/file.js
+++ node-crash-course/basics/file.js
@@ -16,9 +16,32 @@ console.log("last line");
 fs.writeFile("./docs/blog1.txt", "hello,world", () => {
   console.log("file was written");
 });
-fs.writeFile("./docs/blog1.txt", "hello,again", () => {
+fs.writeFile("./docs/blog2.txt", "hello,again", () => {
   console.log("file was written");
 });
 //directories
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder created");
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder deleted");
+  });
+}
 
 //deleting files
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
